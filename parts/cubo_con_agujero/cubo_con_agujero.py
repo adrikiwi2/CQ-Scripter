5832ab7dc691904ad959a3ac34c833fe7c831285
Adrian Hernandez <adrian.hernand2.000@gmail.com>
2025-09-01 16:36:05 +0200

    Add parts/ to .gitignore and remove from tracking

--- parts/cubo_con_agujero/cubo_con_agujero.py
+++
@@ -1,8 +0,0 @@
-import os
-os.makedirs('parts', exist_ok=True)
-
-import cadquery as cq
-
-pieza = cq.Workplane("XY").box(10, 10, 10).faces(">Z").workplane().center(0, 0).hole(4)
-
-cq.exporters.export(pieza, 'parts/cubo_con_agujero/cubo_con_agujero.step')
